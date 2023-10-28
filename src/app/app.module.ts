69aab9ec1bc38b5a9f1d04fb1e3eac910239d59a
Eddy <eddy_ftw@hotmail.com>
2023-10-28 15:12:53 +0200

    Add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ImgGalleryComponent } from './img-gallery/img-gallery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryComponent } from '@daelmaak/ngx-gallery';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavBarComponent,
+    ImgGalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GalleryComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/gallery-item.model.ts
@@ -0,0 +1,65 @@
+export interface GalleryItem {
+    /**
+     * Media url
+     */
+    src: string;
+    /**
+     * Url of media thumbnail
+     */
+    thumbSrc?: string;
+    /**
+     * Alt text for not yet loaded image
+     */
+    alt?: string;
+    /**
+     * Description that is to be shown on the currently displayed gallery item
+     */
+    description?: string;
+    /**
+     * Is asset a video?
+     */
+    video?: boolean;
+    pictureSources?: PictureSource[];
+}
+/**
+ * The whole interface mirrors <source> attributes from the WHATWG spec.
+ */
+export interface PictureSource {
+    /**
+     * src URL
+     */
+    srcset: string;
+    /**
+     * CSS media selector
+     */
+    media?: string;
+    /**
+     * Give hint to browser which src from srcset to pick
+     */
+    sizes?: string;
+    /**
+     * MIME media type
+     */
+    type?: string;
+}
+export interface GalleryItemEvent {
+    /**
+     * Index of the item
+     */
+    index: number;
+    item: GalleryItem;
+    /**
+     * DOM event
+     */
+    event: Event;
+}
+export interface GalleryItemInternal extends GalleryItem {
+    /**
+     * `true` when media couldn't be loaded
+     */
+    _failed?: boolean;
+    /**
+     * `true` when thumbnail couldn't be loaded
+     */
+    _thumbFailed?: boolean;
+}
