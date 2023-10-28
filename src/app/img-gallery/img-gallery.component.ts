3072a1216a5465a2672002a8c7e3889f49cb718c
Eddy Ikeagwy <Eddy.Ikeagwy@aginco.be>
2023-10-28 21:39:32 +0200

    update item display

--- src/app/img-gallery/img-gallery.component.ts
+++ src/app/img-gallery/img-gallery.component.ts
@@ -9,16 +9,16 @@ import { GalleryItem } from '../shared/gallery-item.model';
 
 export class ImgGalleryComponent {
   items: GalleryItem[] = [
-    {
-      src: 'src\app\img-gallery\img-gallery.component.css',
-      thumbSrc:
-        'src\app\img-gallery\img-gallery.component.css',
-    },
-    {
-      src: 'https://cdn.pixabay.com/photo/2017/01/12/02/34/coffee-1973549_960_720.jpg',
-      thumbSrc:
-        'https://cdn.pixabay.com/photo/2017/01/12/02/34/coffee-1973549_960_720.jpg',
-    },
+    // {
+    //   src: 'src\app\img-gallery\img-gallery.component.css',
+    //   thumbSrc:
+    //     'src\app\img-gallery\img-gallery.component.css',
+    // },
+    // {
+    //   src: 'https://cdn.pixabay.com/photo/2017/01/12/02/34/coffee-1973549_960_720.jpg',
+    //   thumbSrc:
+    //     'https://cdn.pixabay.com/photo/2017/01/12/02/34/coffee-1973549_960_720.jpg',
+    // },
     {
       src: 'https://cdn.pixabay.com/photo/2020/06/26/04/40/flower-5341644_960_720.jpg',
       thumbSrc:
