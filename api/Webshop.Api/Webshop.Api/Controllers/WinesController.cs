391a5ead2ee01b7c7e2a2899000afafde59bbdad
Eddy Ikeagwy <Eddy.Ikeagwy@aginco.be>
2023-12-11 14:34:11 +0100

    WIP - add Azure storage upload method

--- api/Webshop.Api/Webshop.Api/Controllers/WinesController.cs
+++ api/Webshop.Api/Webshop.Api/Controllers/WinesController.cs
@@ -7,6 +7,8 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 
 namespace Website.Controllers
 {
@@ -49,5 +51,57 @@ namespace Website.Controllers
         return BadRequest($"Something went wrong adding a wine. {ex.Message} ");
       }
     }
+
+    //private async Task TestImageAzureDownloadFromBlob()
+    //{
+    //  // Create a BlobServiceClient instance
+    //  var blobServiceClient = new BlobServiceClient(new Uri("https://mystorageaccount.blob.core.windows.net"));
+
+    //  // Create the container client
+    //  var containerClient = blobServiceClient.GetBlobContainerClient("mycontainer");
+
+    //  // Download the image blob
+    //  try
+    //  {
+    //    var imageBlob = await containerClient.DownloadBlobAsync("myimage.png");
+    //    var imageBytes = await imageBlob.ReadAllBytesAsync();
+
+    //    // Save the image bytes to a file
+    //    File.WriteAllBytes("image-copy.png", imageBytes);
+    //  }
+    //  catch (Exception ex)
+    //  {
+    //    // Handle the exception
+    //  }
+    //}
+
+    public async Task<string> UploadImageToBlobAsync(IFormFile imageFile)
+    {
+      // Get Azure Storage Account connection string from configuration
+      //string connectionString = _configuration.GetConnectionString("AzureStorageConnection");
+      string connectionString = "Test";
+      // Get a reference to the blob container
+      string containerName = "your-container-name"; // Replace with your Azure Storage container name
+      BlobContainerClient containerClient = new BlobContainerClient(connectionString, containerName);
+
+      // Create the container if it doesn't exist
+      await containerClient.CreateIfNotExistsAsync();
+
+      // Generate a unique name for the blob
+      string blobName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+
+      // Get a reference to the blob
+      BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+      // Upload the image to the blob
+      using (var stream = imageFile.OpenReadStream())
+      {
+        await blobClient.UploadAsync(stream, true);
+      }
+
+      // Return the URL of the uploaded image
+      return blobClient.Uri.ToString();
+    }
+
   }
 }
